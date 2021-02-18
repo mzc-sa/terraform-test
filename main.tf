0b5c7868943f0ffbc72a67d8fa2c1760471530ca
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-02-18 11:04:56 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+//--------------------------------------------------------------------
+// Modules
+
+module "vpc" {
+  source  = "app.terraform.io/MEGA10/vpc/aws"
+  version = "1.0.0"
+
+  name = "Today-vpc"
+  cidr = "10.10.0.0/16"
+  azs = ["ap-northeast-2a", "ap-northeast-2c"]
+  enable_nat_gateway = "true"
+  one_nat_gateway_per_az = "true"
+  single_nat_gateway = "true"
+
+  private_subnets = ["10.10.100.0/24", "10.10.101.0/24"]
+  public_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
+
+  tags = { Terraform = "true", Environment = "dev" }
+}
