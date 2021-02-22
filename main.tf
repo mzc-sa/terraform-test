7697a0d8eb8436afee80fed18c439e746cc60b18
Jaehwan <jhjeong@megazone.com>
2021-02-22 21:45:50 +0900

    update

--- main.tf
+++ main.tf
@@ -5,15 +5,45 @@ module "vpc" {
   source  = "app.terraform.io/MEGA10/vpc/aws"
   version = "1.0.6"
 
-  name = "Today-vpc"
-  cidr = "10.10.0.0/16"
-  azs = ["ap-northeast-2a", "ap-northeast-2c"]
-  enable_nat_gateway = "true"
-  one_nat_gateway_per_az = "true"
-  single_nat_gateway = "true"
+  name = var.name
+  cidr = var.cidr
+  azs  = var.azs
 
-  private_subnets = ["10.10.100.0/24", "10.10.101.0/24"]
-  public_subnets = ["10.10.1.0/24", "10.10.2.0/24"]
+  enable_nat_gateway     = var.enable_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+  single_nat_gateway     = var.single_nat_gateway
 
-  tags = { Terraform = "true", Environment = "dev" }
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  private_subnets  = var.private_subnets
+  public_subnets   = var.public_subnets
+  database_subnets = var.database_subnets
+
+  tags = var.tags
+  igw_tags = {
+    Name = "${var.name}-igw"
+  }
+
+  nat_eip_tags = {
+    Name = "${var.name}-nat-eip"
+  }
+
+  nat_gateway_tags = {
+    Name = "${var.name}-nat-gw"
+  }
+  private_subnet_tags = {
+    Tier = "private"
+  }
+
+  private_route_table_tags = {
+    Name = "${var.name}-private-rt"
+  }
+
+  public_subnet_tags = {
+    Tier = "public"
+  }
+
+  public_route_table_tags = {
+    Name = "${var.name}-public-rt"
+  }
 }
