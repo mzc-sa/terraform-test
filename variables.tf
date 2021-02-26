344b9fc5fedfc1f3408d1f2b175d995944a17231
Jaehwan <jhjeong@megazone.com>
2021-02-26 13:07:17 +0900

    [TEST] 보안그룹 #3

--- variables.tf
+++ variables.tf
@@ -125,8 +125,3 @@ variable "ingress_rules" {
   type        = list(string)
   default     = []
 }
-
-variable "vpc_id" {
-  description = "ID of the VPC where to create security group"
-  type        = string
-}
