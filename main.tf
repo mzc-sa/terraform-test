0de937a9bd732f82479c58420efd949b5266c32c
Jaehwan <jhjeong@megazone.com>
2021-02-26 14:45:53 +0900

    [TEST] 보안그룹 #18

--- main.tf
+++ main.tf
@@ -17,6 +17,19 @@ module "security-group" {
   
   ingress_cidr_blocks      = ["10.10.0.0/16","0.0.0.0/0"]
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "ssh-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
 }
 
 module "s3" {
