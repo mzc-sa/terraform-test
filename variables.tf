0931798cfc33706eb7802f763bc416091d37bba6
Jaehwan <jhjeong@megazone.com>
2021-02-26 14:25:12 +0900

    [TEST] 보안그룹 #10

--- variables.tf
+++ variables.tf
@@ -112,16 +112,3 @@ variable "bucket" {
   type        = string
   default     = null
 }
-
-# Security Group
-variable "ingress_cidr_blocks" {
-  description = "List of IPv4 CIDR ranges to use on all ingress rules"
-  type        = list(string)
-  default     = []
-}
-
-variable "ingress_rules" {
-  description = "List of ingress rules to create by name"
-  type        = list(string)
-  default     = []
-}
