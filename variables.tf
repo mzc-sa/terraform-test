bfec688894a14d1d90edff91b885f7a10275099d
Jaehwan <jhjeong@megazone.com>
2021-02-26 14:20:43 +0900

    [TEST] 보안그룹 #7

--- variables.tf
+++ variables.tf
@@ -114,6 +114,11 @@ variable "bucket" {
 }
 
 # Security Group
+variable "vpc_id" {
+  description = "ID of the VPC where to create security group"
+  type        = string
+}
+
 variable "ingress_cidr_blocks" {
   description = "List of IPv4 CIDR ranges to use on all ingress rules"
   type        = list(string)
