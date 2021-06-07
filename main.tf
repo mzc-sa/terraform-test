34ca67ea55bc76ebd8ad1e68ab9dfc6085a46da0
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-07 20:11:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,38 +8,38 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-module "security-group" {
-  source  = "app.terraform.io/MEGA10/security-group/aws"
-  version = "1.0.4"
+# module "security-group" {
+#   source  = "app.terraform.io/MEGA10/security-group/aws"
+#   version = "1.0.4"
 
-  name   = "${var.name}-sg"
-  vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_id
+#   name   = "${var.name}-sg"
+#   vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_id
   
-  ingress_cidr_blocks      = ["10.10.0.0/16","0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "ssh-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-  egress_rules = ["all-all"]
-}
+#   ingress_cidr_blocks      = ["10.10.0.0/16","0.0.0.0/0"]
+#   ingress_rules       = ["http-80-tcp","https-443-tcp"]
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 8080
+#       to_port     = 8090
+#       protocol    = "tcp"
+#       description = "User-service ports"
+#       cidr_blocks = "10.10.0.0/16"
+#     },
+#     {
+#       rule        = "ssh-tcp"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#   ]
+#   egress_rules = ["all-all"]
+# }
 
-module "s3" {
-  source  = "app.terraform.io/MEGA10/s3/aws"
-  version = "1.0.0"
+# module "s3" {
+#   source  = "app.terraform.io/MEGA10/s3/aws"
+#   version = "1.0.0"
 
-  bucket = var.bucket
-  tags   = var.tags
-}
+#   bucket = var.bucket
+#   tags   = var.tags
+# }
 
 module "vpc" {
   source  = "app.terraform.io/MEGA10/vpc/aws"
