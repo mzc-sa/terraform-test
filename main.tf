956c355f39ae0ada98fa1aabffe4a456dee84acd
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-09 20:28:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,11 +37,27 @@ data "aws_ami" "amazon_linux_2" {
  }
 }
 
+##################################################################
+# Security Group
+##################################################################
+module "security-group" {
+  source  = "app.terraform.io/MEGAZONE-prod/security-group/aws"
+  version = "1.0.4"
+
+  name            = "${var.name}-sg"
+  use_name_prefix = false
+  description     = "Security group for example usage with EC2 instance"
+  vpc_id          = data.aws_vpc.default.id
+
+  ingress_rules       = ["ssh-tcp"]
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  egress_rules  = ["all-all"]
+}
+  
 
 ##################################################################
 # Bastion Host Instance (EC2)
 ##################################################################
-
 resource "aws_eip" "this" {
   vpc      = true
   instance = module.ec2_cluster.id[0]
