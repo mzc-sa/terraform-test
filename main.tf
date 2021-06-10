444552ec6c219092db379c6f3b4527cc184a2b9a
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-10 15:08:48 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,6 @@ module "vpc" {
   name                 = var.name
   cidr                 = var.cidr
   azs                  = var.azs
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
   # subnet
   private_subnets  = var.private_subnets
@@ -39,15 +37,4 @@ module "vpc" {
   public_subnet_tags   = var.public_subnet_tags
   private_subnet_tags  = var.private_subnet_tags
   database_subnet_tags = var.database_subnet_tags
-}
-  
-module "security-group" {
-  source  = "app.terraform.io/MEGAZONE-prod/security-group/aws"
-  version = "1.0.4"
- 
-  name        = "test-sg"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["10.10.0.0/16"]
 }
