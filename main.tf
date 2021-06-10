fac4b6d669f769e6b498adb4f562267b0912705b
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-10 12:52:41 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
 module "vpc" {
   #source  = "git::git@github.com/mzc-sa/terraform-aws-vpc.git//terraform-aws-vpc?ref=1.0.26" 
   source  = "app.terraform.io/MEGAZONE-prod/vpc/aws"
-  version = "1.0.26"
+  version = "1.0.27"
 
   # vpc
   name                 = var.name
