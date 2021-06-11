6ee0a0120fdfd8e60a8da305e29ab0075844d218
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-11 16:28:25 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
 //--------------------------------------------------------------------
 // Modules
 //--------------------------------------------------------------------
+##################################################################
+# Data sources to get VPC, subnet, security group and AMI details
+##################################################################
+data "aws_vpc" "default" {
+  tags = {
+    Environment = "qa"
+  }
+}
+
+
+##################################################################
+# Security Group
+##################################################################
+module "security-group" {
+  source  = "app.terraform.io/MEGAZONE-prod/security-group/aws"
+  version = "1.0.4"
+
+  name            = var.sg_name
+  description     = var.description
+  vpc_id          = data.aws_vpc.default.id
+ 
+  ingress_rules       = ["ssh-tcp"]
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = 
+  
+  tags = var.tags 
+}
