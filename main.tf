2675382a774238417f69ce4d172ee1bd5ce99830
sepiros62 <sepiros62@gmail.com>
2021-06-21 16:19:04 +0900

    [ADD] S3 Upload

--- main.tf
+++ main.tf
@@ -1,87 +1,20 @@
-data "terraform_remote_state" "vpc" {
-  backend = "remote"
-  config = {
-    organization = "MEGA10"
-    workspaces = {
-      name = "terraform-test"
-    }
-  }
-}
 
-# module "security-group" {
-#   source  = "app.terraform.io/MEGA10/security-group/aws"
-#   version = "1.0.4"
+############
+# S3 Bucket
+############
+module "s3" {
+  source  = "app.terraform.io/MEGAZONE-prod/s3/aws"
+  version = "1.0.0"
 
-#   name   = "${var.name}-sg"
-#   vpc_id  = data.terraform_remote_state.vpc.outputs.vpc_id
-  
-#   ingress_cidr_blocks      = ["10.10.0.0/16","0.0.0.0/0"]
-#   ingress_rules       = ["http-80-tcp","https-443-tcp"]
-#   ingress_with_cidr_blocks = [
-#     {
-#       from_port   = 8080
-#       to_port     = 8090
-#       protocol    = "tcp"
-#       description = "User-service ports"
-#       cidr_blocks = "10.10.0.0/16"
-#     },
-#     {
-#       rule        = "ssh-tcp"
-#       cidr_blocks = "0.0.0.0/0"
-#     },
-#   ]
-#   egress_rules = ["all-all"]
-# }
+  bucket        = var.bucket
+  acl           = "private"
+  force_destroy = true
 
-# module "s3" {
-#   source  = "app.terraform.io/MEGA10/s3/aws"
-#   version = "1.0.0"
-
-#   bucket = var.bucket
-#   tags   = var.tags
-# }
-
-module "vpc" {
-  source  = "app.terraform.io/MEGA10/vpc/aws"
-  version = "1.0.10"
-
-  name = var.name
-  cidr = var.cidr
-  azs  = var.azs
-
-  enable_nat_gateway     = var.enable_nat_gateway
-  one_nat_gateway_per_az = var.one_nat_gateway_per_az
-  single_nat_gateway     = var.single_nat_gateway
-
-  private_subnets  = var.private_subnets
-  public_subnets   = var.public_subnets
-  database_subnets = var.database_subnets
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 
   tags = var.tags
-  igw_tags = {
-    Name = "${var.name}-igw"
-  }
-
-  nat_eip_tags = {
-    Name = "${var.name}-nat-eip"
-  }
-
-  nat_gateway_tags = {
-    Name = "${var.name}-nat-gw"
-  }
-  private_subnet_tags = {
-    Tier = "private"
-  }
-
-  private_route_table_tags = {
-    Name = "${var.name}-private-rt"
-  }
-
-  public_subnet_tags = {
-    Tier = "public"
-  }
-
-  public_route_table_tags = {
-    Name = "${var.name}-public-rt"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -1,127 +1,47 @@
-variable "name" {
-  description = "Name to be used on all the resources as identifier"
-  type        = string
-  default     = ""
-}
-
-variable "cidr" {
-  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "azs" {
-  description = "A list of availability zones names or ids in the region"
-  type        = list(string)
-  default     = []
-}
-
-variable "enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+variable "create_bucket" {
+  description = "Controls if S3 bucket should be created"
   type        = bool
-  default     = false
+  default     = true
 }
 
-variable "one_nat_gateway_per_az" {
-  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
-  type        = bool
-  default     = false
-}
-
-variable "single_nat_gateway" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
-  type        = bool
-  default     = false
-}
-
-variable "enable_dns_hostnames" {
-  description = "Should be true to enable DNS hostnames in the VPC"
-  type        = bool
-  default     = false
-}
-
-variable "private_subnets" {
-  description = "A list of private subnets inside the VPC"
-  type        = list(string)
-  default     = []
-}
-
-variable "public_subnets" {
-  description = "A list of public subnets inside the VPC"
-  type        = list(string)
-  default     = []
-}
-
-variable "database_subnets" {
-  description = "A list of database subnets"
-  type        = list(string)
-  default     = []
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "igw_tags" {
-  description = "Additional tags for the internet gateway"
-  type        = map(string)
-  default     = {}
-}
-
-variable "nat_eip_tags" {
-  description = "Additional tags for the NAT EIP"
-  type        = map(string)
-  default     = {}
-}
-
-variable "nat_gateway_tags" {
-  description = "Additional tags for the NAT gateways"
-  type        = map(string)
-  default     = {}
-}
-
-variable "public_route_table_tags" {
-  description = "Additional tags for the public route tables"
-  type        = map(string)
-  default     = {}
-}
-
-variable "public_subnet_tags" {
-  description = "Additional tags for the public subnets"
-  type        = map(string)
-  default     = {}
-}
-
-variable "private_subnet_tags" {
-  description = "Additional tags for the private subnets"
-  type        = map(string)
-  default     = {}
-}
-
-variable "private_route_table_tags" {
-  description = "Additional tags for the private route tables"
-  type        = map(string)
-  default     = {}
-}
-
-# S3
 variable "bucket" {
   description = "(Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name."
   type        = string
   default     = null
 }
 
-# SG
-variable "ingress_rules" {
-  description = "List of ingress rules to create by name"
-  type        = list(string)
-  default     = []
+variable "versioning" {
+  description = "Map containing versioning configuration."
+  type        = map(string)
+  default     = {}
 }
 
-variable "ingress_cidr_blocks" {
-  description = "List of IPv4 CIDR ranges to use on all ingress rules"
-  type        = list(string)
-  default     = []
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "block_public_acls" {
+  description = "Whether Amazon S3 should block public ACLs for this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket."
+  type        = bool
+  default     = false
+}
+
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket."
+  type        = bool
+  default     = false
 }
