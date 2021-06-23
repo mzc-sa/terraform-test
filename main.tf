baef6a6a971f79bd879d089c05d3329fcd3621c9
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-23 20:58:45 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ locals {
 ############
 # ELB Module
 ############
-module "extarnal_elb" {
+module "elb" {
   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
   version = "1.0.3"
    
@@ -56,60 +56,6 @@ module "extarnal_elb" {
 
   vpc_id             = local.vpc_id
   subnets            = var.public_subnets
-  security_groups    = [aws_security_group.http.id]
-
-  target_groups = [
-    {
-      name             = "${var.name}-tg"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      deregistration_delay = 10
-      health_check = {
-        enabled             = true
-        interval            = 30
-        path                = "/"
-        port                = "traffic-port"
-        healthy_threshold   = 3
-        unhealthy_threshold = 3
-        timeout             = 6
-        protocol            = "HTTP"
-        matcher             = "200"
-      }
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port        = 80
-      protocol    = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = var.tags
-  lb_tags = var.lb_tags
-}
-  
-resource "aws_security_group" "http" {
-  name   = "${var.name}-web-sg"
-  vpc_id = data.aws_vpc.default.id
-  
-  tags = {
-    Name = "${var.name}-web-sg"
-  }
-}
-
-module "internal_elb" {
-  source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
-  version = "1.0.3"
- 
-  name               = "${var.name}-alb"
-  internal           = var.internal
-  load_balancer_type = var.load_balancer_type
-
-  vpc_id             = local.vpc_id
-  subnets            = private_subnets
   security_groups    = [aws_security_group.http.id]
 
   target_groups = [
