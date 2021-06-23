0195ce8f0b4936989585e458d95b6fc68ff512e3
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-23 18:10:10 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "vpc_id" {
 
 variable "subnets" {
   description = "A list of subnets to associate with the load balancer. e.g. ['subnet-1a2b3c4d','subnet-1a2b3c4e','subnet-1a2b3c4f']"
-  type        = set(string)
+  type        = list(string)
   default     = null
 }
 
