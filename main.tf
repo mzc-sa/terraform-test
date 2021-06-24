b667a15f0a651fc0a6b63cf9166873ef5517fca1
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-24 16:20:02 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "elb_manual" {
   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
   version = "1.0.3"
   
-  count = var.vpc_id != "" && var.subnets != [] ? 1 : 0
+  count = var.vpc_id == "" && var.subnets == [] ? 0 : 1
   
   name               = "${var.name}-alb"
   internal           = var.internal
