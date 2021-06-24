312819cdb6ec3a32fe7ea8110fdc3cce16a903e7
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-06-24 15:42:47 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,30 +20,41 @@ data "aws_subnet_ids" "public" {
 ################
 # Local Variable
 ################
-locals {
-  vpc_id  = ( var.vpc_id == true  ? var.vpc_id : data.aws_vpc.default.id )
-  subnets = ( var.subnets == true ? var.subnets : data.aws_subnet_ids.public.ids )
+# locals {
+# #   vpc_id  = ( var.vpc_id == true  ? var.vpc_id : data.aws_vpc.default.id )
+# #   subnets = ( var.subnets == true ? var.subnets : data.aws_subnet_ids.public.ids )
 
-  depends_on = [
-    data.aws_vpc.default.id,
-    data.aws_subnet_ids.public.id
-  ]  
-}
+#   depends_on = [
+#     data.aws_vpc.default.id,
+#     data.aws_subnet_ids.public.id
+#   ]  
+# }
 
 
 ############
 # ELB Module
 ############
-module "elb" {
+resource "aws_security_group" "http" {
+  name   = "${var.name}-sg"
+  vpc_id = data.aws_vpc.default.id
+  
+  tags = {
+    Name = "${var.name}-sg"
+  }
+}
+
+module "elb_manual" {
   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
   version = "1.0.3"
-   
+  
+  count = var.vpc_id != null && var.subnets != null ? 1 : 0
+  
   name               = "${var.name}-alb"
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
 
-  vpc_id             = local.vpc_id
-  subnets            = local.subnets
+  vpc_id             = var.vpc_id
+  subnets            = var.subnets
   security_groups    = [aws_security_group.http.id]
 
   target_groups = [
@@ -77,18 +88,52 @@ module "elb" {
 
   tags = var.tags
   lb_tags = var.lb_tags
+}
 
-  depends_on = [
-    local.vpc_id,
-    local.subnets
+module "elb_auto" {
+  source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
+  version = "1.0.3"
+
+    
+  count = var.vpc_id == null || var.subnets == null ? 1 : 0
+  
+  name               = "${var.name}-alb"
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+
+  vpc_id             = data.aws_vpc.default.vpc_id
+  subnets            = data.aws_subnet_ids.public.ids
+  security_groups    = [aws_security_group.http.id]
+
+  target_groups = [
+    {
+      name             = "${var.name}-tg"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200"
+      }
+    }
   ]
-}
-  
-resource "aws_security_group" "http" {
-  name   = "${var.name}-sg"
-  vpc_id = data.aws_vpc.default.id
-  
-  tags = {
-    Name = "${var.name}-sg"
-  }
-}
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = var.tags
+  lb_tags = var.lb_tags
+}  
