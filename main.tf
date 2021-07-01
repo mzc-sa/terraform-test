4a8c0515561af699537ee8c28ca938d2fdcab62d
Jaehwan Jeong <31501015+sepiros62@users.noreply.github.com>
2021-07-01 17:05:27 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ data "aws_vpc" "default" {
   }
 }
 
-data "aws_subnet_ids" "public" {
+data "aws_subnet_ids" "private" {
   vpc_id = data.aws_vpc.default.id
 
   tags = {
-    Tier        = "public"
+    Tier        = "private"
     Environment = "*"
   }
 }
 
 
-############
+#############
 # ELB Module
-############
+#############
 resource "aws_security_group" "http" {
   name   = "${var.name}-sg"
   vpc_id = data.aws_vpc.default.id
@@ -29,7 +29,7 @@ resource "aws_security_group" "http" {
   }
 }
 
-# Custom Subnets 
+# Custom Subnets
 module "elb_manual" {
   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
   version = "1.0.3"
@@ -47,7 +47,7 @@ module "elb_manual" {
   target_groups = [
     {
       name             = "${var.name}-tg"
-      backend_protocol = "HTTP"
+      backend_protocol = "TCP"
       backend_port     = 80
       target_type      = "instance"
       deregistration_delay = 10
@@ -65,11 +65,10 @@ module "elb_manual" {
     }
   ]
 
-  https_listeners = [
+  http_tcp_listeners = [
     {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = var.certificate_arn
+      port        = 80
+      protocol    = "TCP"
       target_group_index = 0
     }
   ]
@@ -90,13 +89,13 @@ module "elb_auto" {
   load_balancer_type = var.load_balancer_type
 
   vpc_id             = data.aws_vpc.default.id
-  subnets            = data.aws_subnet_ids.public.ids
+  subnets            = data.aws_subnet_ids.private.ids
   security_groups    = [aws_security_group.http.id]
 
   target_groups = [
     {
       name             = "${var.name}-tg"
-      backend_protocol = "HTTP"
+      backend_protocol = "TCP"
       backend_port     = 80
       target_type      = "instance"
       deregistration_delay = 10
@@ -114,18 +113,148 @@ module "elb_auto" {
     }
   ]
 
-  https_listeners = [
+  http_tcp_listeners = [
     {
-      port               = 443
-      protocol           = "HTTPS"
-      certificate_arn    = var.certificate_arn
+      port               = 80
+      protocol           = "TCP"
       target_group_index = 0
     }
   ]
 
   tags = var.tags
   lb_tags = var.lb_tags
-} 
+}  
+
+
+
+# ###############
+# # Data sources 
+# ###############
+# data "aws_vpc" "default" {
+#   tags = {
+#     Environment = "*"
+#   }
+# }
+
+# data "aws_subnet_ids" "public" {
+#   vpc_id = data.aws_vpc.default.id
+
+#   tags = {
+#     Tier        = "public"
+#     Environment = "*"
+#   }
+# }
+
+
+# ############
+# # ELB Module
+# ############
+# resource "aws_security_group" "http" {
+#   name   = "${var.name}-sg"
+#   vpc_id = data.aws_vpc.default.id
+  
+#   tags = {
+#     Name = "${var.name}-sg"
+#   }
+# }
+
+# # Custom Subnets 
+# module "elb_manual" {
+#   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
+#   version = "1.0.3"
+  
+#   count = var.vpc_id != " " && var.subnets != [] ? 1 : 0
+  
+#   name               = "${var.name}-alb"
+#   internal           = var.internal
+#   load_balancer_type = var.load_balancer_type
+
+#   vpc_id             = var.vpc_id
+#   subnets            = var.subnets
+#   security_groups    = [aws_security_group.http.id]
+
+#   target_groups = [
+#     {
+#       name             = "${var.name}-tg"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#       deregistration_delay = 10
+#       health_check = {
+#         enabled             = true
+#         interval            = 30
+#         path                = "/"
+#         port                = "traffic-port"
+#         healthy_threshold   = 3
+#         unhealthy_threshold = 3
+#         timeout             = 6
+#         protocol            = "HTTP"
+#         matcher             = "200"
+#       }
+#     }
+#   ]
+
+#   https_listeners = [
+#     {
+#       port               = 443
+#       protocol           = "HTTPS"
+#       certificate_arn    = var.certificate_arn
+#       target_group_index = 0
+#     }
+#   ]
+
+#   tags = var.tags
+#   lb_tags = var.lb_tags
+# }
+
+# # Data Source Subnets
+# module "elb_auto" {
+#   source  = "app.terraform.io/MEGAZONE-prod/elb/aws"
+#   version = "1.0.3"
+
+#   count = var.vpc_id != " " && var.subnets != [] ? 0 : 1
+  
+#   name               = "${var.name}-alb"
+#   internal           = var.internal
+#   load_balancer_type = var.load_balancer_type
+
+#   vpc_id             = data.aws_vpc.default.id
+#   subnets            = data.aws_subnet_ids.public.ids
+#   security_groups    = [aws_security_group.http.id]
+
+#   target_groups = [
+#     {
+#       name             = "${var.name}-tg"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#       deregistration_delay = 10
+#       health_check = {
+#         enabled             = true
+#         interval            = 30
+#         path                = "/"
+#         port                = "traffic-port"
+#         healthy_threshold   = 3
+#         unhealthy_threshold = 3
+#         timeout             = 6
+#         protocol            = "HTTP"
+#         matcher             = "200"
+#       }
+#     }
+#   ]
+
+#   https_listeners = [
+#     {
+#       port               = 443
+#       protocol           = "HTTPS"
+#       certificate_arn    = var.certificate_arn
+#       target_group_index = 0
+#     }
+#   ]
+
+#   tags = var.tags
+#   lb_tags = var.lb_tags
+# } 
 
 
 ###################################### Internal ALB (HTTP) 
